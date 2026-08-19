831b97506f4ba4d7cba12f0eaaeef26ec8433eef
dhanushkumar-merndev <dhanushkumar.merndev@gmail.com>
2026-08-19 16:02:19 +0530

    chore: remove .vercelignore file

--- .pnpm-store/v11/projects/bd7162cb468d431fb4e3f96bfdb37733/src/lib/utils/cn.ts
+++
@@ -1,7 +0,0 @@
-import { clsx, type ClassValue } from 'clsx';
-import { twMerge } from 'tailwind-merge';
-
-/** Conditional class names with Tailwind conflict resolution. */
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
